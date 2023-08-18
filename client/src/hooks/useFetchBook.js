fbf367a1e2dce7b68fd8586d1805504ce3ccb7b1
Julien Devlin <juliendevlin@gmail.com>
2023-08-18 15:30:29 -0400

    add custom hook to fetch data for a single book with its isbn

---
+++ client/src/hooks/useFetchBook.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+function useFetchBook(isbn) {
+  const [book, setBook] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBook = async (isbn) => {
+      setIsLoading(true);
+  
+      try {
+        const response = await fetch(`http://localhost:3001/books/${isbn}`);
+        const data = await response.json();
+  
+        setIsLoading(false);
+        setBook(data);
+      } catch(err) {
+        setIsLoading(false);
+        setError(err);
+      }
+    };
+
+    fetchBook(isbn);
+  }, [isbn]);
+
+  return { book, isLoading, error };
+}
+
+export default useFetchBook;
