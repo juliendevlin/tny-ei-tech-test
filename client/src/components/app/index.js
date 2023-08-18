db0bdc7ea76929ff5ff7a80d817cba058d31b220
Julien Devlin <juliendevlin@gmail.com>
2023-08-18 13:58:12 -0400

    add search route

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Search from '../search'
+import Search from '../search'
 // import Detail from '../detail'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Search />} /> */}
+        <Route path="/" element={<Search />} />
         {/* <Route path="/books/:isbn" element={<Detail />} /> */}
       </Routes>
     </BrowserRouter>
