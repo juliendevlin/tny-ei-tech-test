bbef4945784970b38b080f674cd709d7d399d30b
Julien Devlin <juliendevlin@gmail.com>
2023-08-19 21:16:52 -0400

    add error handling to get routes

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -3,19 +3,42 @@ const { getBooks, getBook } = require('../db');
 const router = express.Router();
 
 function create_router(db) {
-  router.get('/books/:isbn', async (req, res) => {
+  router.get('/books/:isbn', async (req, res, next) => {
     const { isbn } = req.params;
-    const books = await getBook(db, isbn);
-    res.json(books);
+
+    try {
+      const book = await getBook(db, isbn);
+
+      if (book === undefined) {
+        return next({
+          log: 'Query did not find a match for requested ISBN',
+          status: 404,
+          message: {err: 'Requested resource does not exist'},
+        });
+      }
+      else {
+        return res.json(book);
+      }
+    } catch (err) {
+      return next({
+        log: `Error while querying database - ${err}`,
+        status: 400,
+        message: {err: 'Failed to retrieve resource'},
+      });
+    }
   });
 
   router.get('/books', async (req, res) => {
-    const books = await getBooks(db);
-    res.json(books);
-  });
-  
-  router.get('/', (req, res) => {
-    res.send('hello, world');
+    try {
+      const books = await getBooks(db);
+      res.json(books);
+    } catch (err) {
+      return next({
+        log: `Error while querying database - ${err}`,
+        status: 400,
+        message: {err: 'Failed to retrieve resource'},
+      });
+    }
   });
 
   return router;
